282deed2fdedaa94f7af3aca7f3e621854bcc299
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-05-03 13:20:23 +0200

    init first task: team introduction

---
+++ vorstellung.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Political Party Classification')
+
+st.header('Erste Aufgabe: Vorstellung')
+
+st.subheader('Unser Team')
+
+st.text('Hallo zusammen,\nwir sind Jan Ackermann, Simon Müller und Dominik Rolof.\nWir studieren Wirtschaftsinformatik und wünschen uns allen ein erfolgreiches Semester.')
+
