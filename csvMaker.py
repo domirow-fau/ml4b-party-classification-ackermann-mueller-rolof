b91f5af7a6fd89fd00d57a862a29ce56b9eebe5c
SimonM211 <92990663+SimonM211@users.noreply.github.com>
2022-06-01 21:35:39 +0200

    CSV added

--- csvMaker.py
+++ csvMaker.py
@@ -2,9 +2,8 @@ import json_lines as jl
 import os
 import csv
 
-with open('tweets.csv', 'w', encoding="utf-8") as file:
+with open('tweets.csv', 'w', newline='', encoding="utf-8") as file:
     writer = csv.writer(file)
-    writer.writerow(["Party", "Tweet"])
 
     files = os.listdir('data')
 
@@ -15,9 +14,10 @@ with open('tweets.csv', 'w', encoding="utf-8") as file:
 
         with jl.open('data\\' + file) as data:
             for item in data:
-                for tweet in item['response']['data']:
-                    text.append(tweet['text'])
-                break
+                if 'data' in item['response']:
+                    for tweet in item['response']['data']:
+                        text.append(tweet['text'])
+                    break
 
         for item in text:
             print(item + "\n")
