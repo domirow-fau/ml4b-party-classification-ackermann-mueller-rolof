e3533f5c54c4ecd478c8be38ace9ec59113ae6d8
JohannesA15 <104435731+JohannesA15@users.noreply.github.com>
2022-07-04 17:17:34 +0200

    Create Data_Exploration.py

---
+++ pages/Data_Exploration.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from PIL import Image
+import json as js
+
+st.title('Data Exploration')
+
+st.header('Unser Datensatz')
+
+st.subheader('Auszug aus dem Twitter-Datensatz')
+##
+file = open('Philipp_Amthor.jl')
+data = js.load(file)
+
+if st.button('Beispiel eines Datensatzes'):
+    st.markdown(data)
+option = st.selectbox('Möchtest du sehen, wem der Datensatz gehört?', ('Bitte auswählen...', 'Ja', 'Nein'))
+if (option == 'Ja'):
+    st.text('Du wolltest es nicht anders...')
+    st.image('https://img-9gag-fun.9cache.com/photo/aGgXrwz_460s.jpg')
+elif (option == 'Bitte auswählen...'):
+    st.text('Bitte wähle Ja oder Nein')
+else:
+    st.text('Dann halt nicht...')
+
+file.close()
+##
+
+st.subheader('Aus 8GB .jl wird 500MB .csv ...')
+
+st.text('--csv einfügen--')
+
+st.header('WordClouds')
+
+image_titles = ["CDU", "CSU", "SPD", "Die_Gruenen", "FDP", "AfD", "Die_Linke", "Fraktionslos"]
+
+for title in image_titles:
+	st.subheader(' \n' + title + ' - WordCloud')
+	image = Image.open('' + title + '_wordcloud.png')
+	st.image(image)
