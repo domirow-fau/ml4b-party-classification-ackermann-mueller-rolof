65f477c6967970bbc96b3926818ff4288b096dd8
JohannesA15 <104435731+JohannesA15@users.noreply.github.com>
2022-07-04 17:36:55 +0200

    Update Data_Exploration.py

--- pages/Data_Exploration.py
+++ pages/Data_Exploration.py
@@ -8,7 +8,7 @@ st.header('Unser Datensatz')
 
 st.subheader('Auszug aus dem Twitter-Datensatz')
 ##
-file = open('ml4b-party-classification-ackermann-mueller-rolof/Philipp_Amthor.jl')
+file = open('domirow-fau/ml4b-party-classification-ackermann-mueller-rolof/Philipp_Amthor.jl')
 data = js.load(file)
 
 if st.button('Beispiel eines Datensatzes'):
@@ -35,5 +35,5 @@ image_titles = ["CDU", "CSU", "SPD", "Die_Gruenen", "FDP", "AfD", "Die_Linke", "
 
 for title in image_titles:
 	st.subheader(' \n' + title + ' - WordCloud')
-	image = Image.open('ml4b-party-classification-ackermann-mueller-rolof' + title + '_wordcloud.png')
+	image = Image.open('domirow-fau/ml4b-party-classification-ackermann-mueller-rolof/pages/wordclouds' + title + '_wordcloud.png')
 	st.image(image)
