b2bc88dc8ac9d0243519ff73888d9eeaa762cb14
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-06 20:44:09 +0200

    Add files via upload

---
+++ pages/partyClassificationLoad.py
@@ -0,0 +1,32 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+import pandas as pd
+import _pickle as cPickle
+
+text = "test"
+
+df = pd.read_csv('tweets.csv')
+df = df.fillna('')
+processed_features = df.iloc[:,1].values
+labels = df.iloc[:, 0].values
+vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
+processed_features = vectorizer.fit_transform(processed_features).toarray()
+X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
+
+print("Naive Bayes")
+with open('NBsave.pkl', 'rb') as fid:
+    clf = cPickle.load(fid)
+    print("Partei: " + str(clf.predict(vectorizer.transform([text]))) + "\n")
+
+print("Random Forest")
+with open('RFsave.pkl', 'rb') as fid:
+    clf = cPickle.load(fid)
+    print("Partei: " + str(clf.predict(vectorizer.transform([text]))) + "\n")
+
+print("Support Vector Machines")
+with open('SVMsave.pkl', 'rb') as fid:
+    clf = cPickle.load(fid)
+    print("Partei: " + str(clf.predict(vectorizer.transform([text]).toarray())) + "\n")
