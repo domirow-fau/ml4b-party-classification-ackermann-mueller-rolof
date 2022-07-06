67c6f213c7cc39f81c76f7df0b7096f98f325d23
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-06 20:55:24 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -13,7 +13,7 @@ df = pd.read_csv('tweets.csv')
 df = df.fillna('')
 processed_features = df.iloc[:,1].values
 labels = df.iloc[:, 0].values
-vectorizer = TfidfVectorizer (max_features=1000, min_df=7, max_df=0.8)
+vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
 processed_features = vectorizer.fit_transform(processed_features).toarray()
 X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
@@ -21,6 +21,10 @@ st.write("Naive Bayes")
 with open('NBsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
+    
+vectorizer = TfidfVectorizer (max_features=1520, min_df=7, max_df=0.8)
+processed_features = vectorizer.fit_transform(processed_features).toarray()
+X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
 st.write("Random Forest")
 with open('RFsave.pkl', 'rb') as fid:
