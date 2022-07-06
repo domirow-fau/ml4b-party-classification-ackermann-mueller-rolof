b85de6c9bbdbce9d5672042732bab16a615a31a8
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-06 20:56:11 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -23,9 +23,6 @@ with open('NBsave.pkl', 'rb') as fid:
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
     
 vectorizer = TfidfVectorizer (max_features=1520, min_df=7, max_df=0.8)
-processed_features = vectorizer.fit_transform(processed_features).toarray()
-X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
-
 st.write("Random Forest")
 with open('RFsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
