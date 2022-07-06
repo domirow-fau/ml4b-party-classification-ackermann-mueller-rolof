52b90d2ec0782d83d33429fc8a8adce64cce11ad
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-06 20:49:54 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -17,17 +17,17 @@ vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
 processed_features = vectorizer.fit_transform(processed_features).toarray()
 X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
-print("Naive Bayes")
+st.write("Naive Bayes")
 with open('NBsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
 
-print("Random Forest")
+st.write("Random Forest")
 with open('RFsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
 
-print("Support Vector Machines")
+st.write("Support Vector Machines")
 with open('SVMsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]).toarray())))
