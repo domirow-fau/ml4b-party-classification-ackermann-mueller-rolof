2f8dc203b2da0a138109d0907e029b7fc117825d
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-06 20:59:31 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -14,7 +14,7 @@ df = df.fillna('')
 processed_features = df.iloc[:,1].values
 labels = df.iloc[:, 0].values
 vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
-processed_features = vectorizer.fit_transform(processed_features).toarray()
+processed_features = vectorizer.transform(processed_features).toarray()
 X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
 st.write("Naive Bayes")
@@ -22,7 +22,6 @@ with open('NBsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
     st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
     
-vectorizer = TfidfVectorizer (max_features=1520, min_df=7, max_df=0.8)
 st.write("Random Forest")
 with open('RFsave.pkl', 'rb') as fid:
     clf = cPickle.load(fid)
