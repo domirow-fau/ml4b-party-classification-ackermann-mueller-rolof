4ce5cad61dc6b417fb07f1dde9c28e9dfee9ce15
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-07 12:37:21 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -25,7 +25,9 @@ if  text != "":
     vectorizer1 = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
     vectorizer2 = TfidfVectorizer (max_features=1520, min_df=7, max_df=0.8)
     vectorizer3 = TfidfVectorizer (max_features=802, min_df=7, max_df=0.8)
-    processed_features = vectorizer.fit_transform(processed_features).toarray()
+    processed_features = vectorizer1.fit_transform(processed_features).toarray()
+    processed_features = vectorizer2.fit_transform(processed_features).toarray()
+    processed_features = vectorizer3.fit_transform(processed_features).toarray()
     X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
     if option == "Naive Bayes":
