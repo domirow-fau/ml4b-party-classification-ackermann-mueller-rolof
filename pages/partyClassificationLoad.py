a5e0f7480f77c163eef9824560a1fcc1bbd98db0
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-07 12:24:39 +0200

    Update partyClassificationLoad.py

--- pages/partyClassificationLoad.py
+++ pages/partyClassificationLoad.py
@@ -7,27 +7,45 @@ import pandas as pd
 import _pickle as cPickle
 import streamlit as st
 
-text = "test"
 
-df = pd.read_csv('tweets.csv')
-df = df.fillna('')
-processed_features = df.iloc[:,1].values
-labels = df.iloc[:, 0].values
-vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
-processed_features = vectorizer.fit_transform(processed_features).toarray()
-X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
+st.header("Political Party Classification")
+st.subheader("Vortrainierte Analyse von Tweets")
+st.text("In dieser Sektion wird der Tweet\nanhand vortrainierter Modelle analysiert")
 
-st.write("Naive Bayes")
-with open('NBsave.pkl', 'rb') as fid:
-    clf = cPickle.load(fid)
-    st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
+option = st.radio("Bitte gewünschte Analyse-Methode wählen:", ["Naive Bayes", "Random Forest", "Support Vector Machines"], 0)
 
-st.write("Random Forest")
-with open('RFsave.pkl', 'rb') as fid:
-    clf = cPickle.load(fid)
-    st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
+text = st.text_input("Bitte den Tweet eingeben...", "")
 
-st.write("Support Vector Machines")
-with open('SVMsave.pkl', 'rb') as fid:
-    clf = cPickle.load(fid)
-    st.write("Partei: " + str(clf.predict(vectorizer.transform([text]).toarray())))
+if  text != "":
+
+    df = pd.read_csv('tweets.csv')
+    df = df.fillna('')
+    processed_features = df.iloc[:,1].values
+    labels = df.iloc[:, 0].values
+    vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
+    processed_features = vectorizer.fit_transform(processed_features).toarray()
+    X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
+
+    if option == "Naive Bayes":
+        print("Naive Bayes")
+        st.write("Naive Bayes")
+        with open('NBsave.pkl', 'rb') as fid:
+            clf = cPickle.load(fid)
+            #print("Partei: " + str(clf.predict(vectorizer.transform([text]))) + "\n")
+            st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))) + "\n")
+
+    elif option == "Random Forest":
+        print("Random Forest")
+        st.write("Random Forest")
+        with open('RFsave.pkl', 'rb') as fid:
+            clf = cPickle.load(fid)
+            #print("Partei: " + str(clf.predict(vectorizer.transform([text]))) + "\n")
+            st.write("Partei: " + str(clf.predict(vectorizer.transform([text]))))
+
+    elif option == "Support Vector Machines":
+        print("Support Vector Machines")
+        st.write("Support Vector Machines")
+        with open('SVMsave.pkl', 'rb') as fid:
+            clf = cPickle.load(fid)
+            #print("Partei: " + str(clf.predict(vectorizer.transform([text]).toarray())) + "\n")
+            st.write("Partei: " + str(clf.predict(vectorizer.transform([text]).toarray())))
