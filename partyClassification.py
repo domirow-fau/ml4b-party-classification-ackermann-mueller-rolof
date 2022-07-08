b92aac6ff3ecfb79731b1b2c9d36071cf3d33593
SimonM211 <92990663+SimonM211@users.noreply.github.com>
2022-07-08 10:58:32 +0200

    Update partyClassification.py

--- partyClassification.py
+++ partyClassification.py
@@ -9,7 +9,7 @@ import pandas as pd
 df = pd.read_csv('tweets.csv')
 processed_features = df.iloc[:,1].values
 labels = df.iloc[:, 0].values
-vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
+vectorizer = TfidfVectorizer (max_features=2500, min_df=4, max_df=0.8)
 processed_features = vectorizer.fit_transform(processed_features).toarray()
 X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.1, random_state=0)
 
